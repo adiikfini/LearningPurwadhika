98cfeda474894e69718e15c4ee471bbf78f078d3
adiikfini <adi.ikfini@gmail.com>
2025-10-07 23:17:17 +0700

    feat: implement second smallest number function and add new file for future exercises

--- Fundamental/Day5/Soal3.js
+++ Fundamental/Day5/Soal3.js
@@ -1,3 +1,20 @@
 /* Write a function from a given array of numbers and return the second smallest number
 a. Example : numbers = [5, 3, 1, 7, 2, 6] → 2 */
+let numbers = [5, 3, 1, 7, 2, 6];
+function secondSmallestNumber(numbers) {
+    let bag = 0;
+    for (let index = 0; index < numbers.length; index++) {
+        for (let j = 0; j < numbers.length; j++) {
+            //min
+            if (numbers[index] > numbers[j]) {
+                //swap
+                bag = numbers[j];
+                numbers[j] = numbers[index];
+                numbers[index] = bag;
+            }
+        }
+    }
+    console.log(numbers[numbers.length-2]);
+}
 
+secondSmallestNumber(numbers);
