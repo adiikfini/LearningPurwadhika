3ebc42967e6610bc4e7fa4709c50cb4b463151b4
adiikfini <adi.ikfini@gmail.com>
2025-10-13 21:45:22 +0700

    feat: implement API data fetching with async/await and define Post interface

---
+++ Fundamental/Day8/Fetch.ts
@@ -0,0 +1,45 @@
+const API_URL: string = 'https://jsonplaceholder.typicode.com/posts';
+interface Post {
+    userId: number;
+    id: number;
+    title: string;
+    body: string;
+}
+
+console.log("Status: Sedang memulai pengambilan data...");
+// fetch()
+// fetch(API_URL)
+//     .then((response: Response) => {
+//         if (!response.ok) {
+//             throw new Error(`HTTP Error! Status: ${response.status}`);// lempar Error jika tidak ada respone
+//         }
+//         return response.json(); //respone berisi Data
+//     })
+//     .then((data: Post[]) => {
+//         const userProfile: Post[] = data;
+//         console.log("Status: Successfully");
+//         console.log(`userId: ${userProfile[0].userId}`);
+//         console.log(`id: ${userProfile[0].id}`);
+//         console.log(`title: ${userProfile[0].title}`);
+//         console.log(`body: ${userProfile[0].body}`);
+//     })
+//     .catch((error: Error) => {//Cek Error
+//         console.error("ERROR");
+//         console.error(`Detail Error: ${error.message}`);
+//     })
+//     .finally(() => {//END
+//         console.log("\nStatus: Proses pengambilan data selesai.");
+//     });
+
+
+const apihandleAsyncAwait = async () => {
+    try {
+        const response = await fetch(API_URL);
+        const Data = await response.json(); 
+        console.log(Data);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+console.log(apihandleAsyncAwait());
