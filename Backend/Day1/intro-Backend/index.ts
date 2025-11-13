8aa34d360b226aa2771794c02e0837838c528b35
adi <adi.ikfini@gmail.com>
2025-11-13 20:33:17 +0700

    feat: update server implementation with API endpoints and add .gitignore for dist directory

--- Backend/Day1/intro-Backend/index.ts
+++ Backend/Day1/intro-Backend/index.ts
@@ -1,9 +1,25 @@
-import http from "http";
+import http, { request } from "http";
 
 const PORT = 3000;
 
-const server = http.createServer();
+const server = http.createServer((request,response) => {
+    if(request.url === "/api" && request.method === "GET") {
+        response.writeHead(200);
+        response.write("Wecome to my API");
+        response.end();
+    } else if (request.url === "/users" && request.method === "GET") {
+        const users = [
+            {id: 1, name: "John Doe"},
+            {id: 2, name: "Jane Smith"},
+            {id: 3, name: "Alice Johnson"}
+        ];
+        response.writeHead(200);
+        response.write(JSON.stringify(users));
+        response.end();
+    }
+});
 
 server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
